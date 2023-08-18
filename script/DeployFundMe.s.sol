2454aefff2659f0949420f0c4c3910977b2b4c4e
theirrationalone <anilkushwaha289@gmail.com>
2023-08-18 17:47:14 +0530

    imports separated 🗡

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -3,11 +3,12 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+
 import {FundMe} from "../src/FundMe.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
-    function run() external returns (FundMe) {
+    function run() external returns (FundMe, HelperConfig) {
         HelperConfig config = new HelperConfig();
         (address priceFeedAddress, uint256 privateKey) = config.activeNetworkConfig();
 
@@ -15,6 +16,6 @@ contract DeployFundMe is Script {
         FundMe fundMe = new FundMe(priceFeedAddress);
         vm.stopBroadcast();
 
-        return fundMe;
+        return (fundMe, config);
     }
 }
